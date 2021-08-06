d6fc497bec645ec3c8bcd3413c723a90a5cbb222
DABND <grigorenko.david@outlook.com>
2021-08-06 16:43:14 +0300

    Remove unused imports.

--- models.py
+++ models.py
@@ -1,13 +1,6 @@
-from datetime import datetime, timedelta, time
-from typing import Tuple
-from sqlalchemy import Column, create_engine, ForeignKey, MetaData, delete
-from sqlalchemy.sql.functions import func
-from sqlalchemy.sql.expression import extract, select
-from sqlalchemy.sql.sqltypes import DateTime, Float, Integer, String, BigInteger, Interval
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime, Integer, String, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import sessionmaker
-from aiogram import types
 
 
 Base = declarative_base()
@@ -26,12 +19,11 @@ class Chat(Base):
     id = Column(BigInteger, primary_key=True)
     title = Column(String)
 
+
 class Reply(Base):
     __tablename__ = "Reply"
-
     id = Column(Integer, primary_key=True)
     time = Column(DateTime)
     delta = Column(Integer)
-    employee = Column(Integer, ForeignKey('Employee.id'))
-    chat = Column(BigInteger, ForeignKey('Chat.id'))
-
+    employee = Column(Integer, ForeignKey(Employee.id))
+    chat = Column(BigInteger, ForeignKey(Chat.id))
