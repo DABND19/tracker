b6cb02a45fcc5f3a875d56edf03fb40c981d20a3
DABND <grigorenko.david@outlook.com>
2021-08-07 17:30:01 +0300

    on_startup fix.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from loader import engine, Session, dp, chats_store
 
 
 async def on_startup():
-    with Session() as session:
+    async with Session() as session:
         await session.run_sync(metadata.create_all(engine))
         await chats_store.load(session)
 
