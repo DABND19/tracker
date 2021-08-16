e9621cf508fe858d5be1fdbfbe8a8a9840a629b5
DABND <grigorenko.david@outlook.com>
2021-08-16 17:07:30 +0300

    Remove webhook deleting.

--- app.py
+++ app.py
@@ -8,12 +8,9 @@ from data.config import WEBHOOK_URL
 
 
 async def on_startup(*args):
-    webhook = await bot.get_webhook_info()
-    if webhook.url != WEBHOOK_URL:
-        if not webhook.url:
-            await bot.delete_webhook()
-        else:
-            logging.warning(f"Webhook is already started on: {webhook.url}")
+    webhook_info = await bot.get_webhook_info()
+    logging.info(webhook_info)
+
     await bot.set_webhook(WEBHOOK_URL, certificate=open("cert.pem", "rb"))
 
     async with engine.begin() as connection:
@@ -22,16 +19,12 @@ async def on_startup(*args):
         await chats_store.load(session)
 
 
-async def on_shutdown(*args):
-    await bot.delete_webhook()
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     executor.start_webhook(
         dispatcher=dp,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
         skip_updates=True,
         webhook_path="/",
         host="0.0.0.0",
